8f07019dbcedff07b4c72dd5245379bba399f7f0
root <zsb@pdx.edu>
2017-07-27 15:51:09 -0700

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_help.xml

--- app/src/main/java/com/example/root/qtv1/Admin.java
+++ app/src/main/java/com/example/root/qtv1/Admin.java
@@ -1,8 +1,12 @@
 package com.example.root.qtv1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.File;
 
 public class Admin extends AppCompatActivity {
 
@@ -13,12 +17,14 @@ public class Admin extends AppCompatActivity {
     }
 
     //deletes file for user specified
-    public void onDeleteTap() {
-
+    public void onDeleteTap(Context context, String username) {
+        File file = context.getFileStreamPath(username);
+        if(file.exists())
+            deleteFile(username);
     }
 
     //returns to the home screen
-    public void onReturnTap() {
+    public void onReturnTap(View v) {
         Intent ret = new Intent(Admin.this, MainActivity.class);
         startActivity(ret);
     }
