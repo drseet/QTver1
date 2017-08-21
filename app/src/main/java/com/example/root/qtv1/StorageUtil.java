b234ba9482d9d9998c862e02fd42735d7289e4ee
root <zsb@pdx.edu>
2017-08-21 16:57:20 -0700

    added average storage

--- app/src/main/java/com/example/root/qtv1/StorageUtil.java
+++ app/src/main/java/com/example/root/qtv1/StorageUtil.java
@@ -18,7 +18,7 @@ each user stored as an object in a file that is named after the username
 class StorageUtil extends AppCompatActivity implements java.io.Serializable {
 
     MessageDigest md;
-
+    int total, sessions;
 
     //used to hash the user's password
     protected String hash(String input) {
@@ -73,7 +73,6 @@ class StorageUtil extends AppCompatActivity implements java.io.Serializable {
 
     protected void storeTotal(String username, int duration) {
         File file = getFileStreamPath(username);
-        int total;
 
         if (file.exists()) {
             file = getFileStreamPath(username + "_qt_total");
@@ -106,21 +105,19 @@ class StorageUtil extends AppCompatActivity implements java.io.Serializable {
 
     protected void storeSessionCount(String username) {
         File file = getFileStreamPath(username);
-        int sessions;
 
         if (file.exists()) {
             file = getFileStreamPath(username + "_qt_sessions");
             if (file.exists()) {
                 //get int from file and add 1 to it- delete old file and create new with sum
                 try {
-                    //pull previous number from total file, close and delete the file
+                    //pull previous number from total file (+1) close and delete the file
                     FileInputStream fis = openFileInput(username + "_qt_sessions");
                     sessions = ((fis.read()) + 1);
                     fis.close();
                     deleteFile(username + "_qt_sessions");
-                    //add one to sessions
-                    //open/create file and save data
 
+                    //open/create file and save updated session data
                     FileOutputStream fos = openFileOutput(username + "_qt_sessions", Context.MODE_PRIVATE);
                     fos.write(sessions);
                     fos.close();
@@ -136,10 +133,36 @@ class StorageUtil extends AppCompatActivity implements java.io.Serializable {
         }
     }
 
+    /*
+    gather total quiet time and amt of sessions and calc avg. delete old file and replace w/
+    new avg data
+    */
+    protected void storeSessionAvg(String username) {
+        int avg = this.total / this.sessions;
+
+        Log.v("Storage", "avg: " + avg);
+
+        File file = getFileStreamPath(username);
+        if (!file.exists()) {
+            Log.v("Storage", "ERROR: user file not found!");
+            return;
+        }
+        deleteFile(username + "_qt_avg");
+        try {
+            FileOutputStream fos = openFileOutput(username + "_qt_avg", Context.MODE_PRIVATE);
+            fos.write(avg);
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.v("Storage", "ERROR: unable to save qt avg");
+        }
+    }
+
     //store quiet time stats in private files
     protected void storeUserStats(String username, int duration) {
         storeTotal(username, duration);
         storeSessionCount(username);
+        storeSessionAvg(username);
 
     }
 
